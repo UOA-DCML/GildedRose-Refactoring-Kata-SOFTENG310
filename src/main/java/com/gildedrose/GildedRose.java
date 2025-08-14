7284924010781b0d213968697c4ff6d07d6a119b
oorjagandhi <oorjagandhi@gmail.com>
2025-08-14 14:47:06 +1200

    refactor: extract quality bounds checking into helper methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,24 +11,24 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
+                if (canDecreaseQuality(items[i])) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (canIncreaseQuality(items[i])) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (canIncreaseQuality(items[i])) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (canIncreaseQuality(items[i])) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -43,7 +43,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
+                        if (canDecreaseQuality(items[i])) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
@@ -52,11 +52,21 @@ class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (canIncreaseQuality(items[i])) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
+    }
+
+    // Helper method to check if quality can be increased (max 50)
+    private boolean canIncreaseQuality(Item item) {
+        return item.quality < 50;
+    }
+
+    // Helper method to check if quality can be decreased (min 0)
+    private boolean canDecreaseQuality(Item item) {
+        return item.quality > 0;
     }
 }
